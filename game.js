ac11fc3b69ee1c43c1eab41554371a98cf97b52b
Stephanie <lopezlstephanie@gmail.com>
2016-01-13 16:28:25 -0800

    expanded explanation for project in the  README.md file and  added counter for the questions in game.js

--- game.js
+++ game.js
@@ -1,61 +1,57 @@
+   var counter = 0
+
+
    var userName = prompt("Hello what's your name?");
-    console.log("This is the userName" + " " + userName);
-    alert("Hi" + " " + userName + "!");
-
-   var game = prompt(userName + " " + "could you answer some questions for me? \n yes or no");
-    console.log("Answer to game" + " " + game);
-
-    if (game === "yes" || game === "YES"){
-      console.log("yes is correct");
-      alert("Great" + " " + userName + " " + "let's get started!");
-    } else if (game === "no" || game ==="NO") {
-      console.log("no is incorrect");
-      alert("Sucks for you" + " " + userName + " " + "you have to answer anyway");
-    } else if (game !== "yes" || "YES" || "no" || "NO"){
-      console.log("incorrect input");
-      alert("incorrect input");
-    };
+    console.log("This is the users name" + " " + userName);
+    alert("Hi" + " " + userName + " " + "I'm going to ask you a couple of yes or no questions so you can get to know me.");
 
 
-   var ques1 = prompt("Do you like pizza? \n yes or no");
+   var ques1 = prompt("Do I have any pets?");
     console.log("Answer to ques1" + " " + ques1);
 
-   if (ques1 === "yes" ||  ques1 === "YES"){
-    console.log("yes is correct");
-    alert(userName + " " + "you're awesome");
-  } else if (ques1 === "no" || ques1 === "NO") {
-    console.log("no is incorrect");
-    alert(userName + " " +"you are missing out");
-  } else if (ques1 !== "yes" || "YES" || "no" || "NO"){
-    console.log("incorrect input");
-    alert("incorrect input");
-  };
+   if (ques1 === "no" ||  ques1 === "NO"){
+    console.log("no is correct");
+    alert(userName + " " + "you're right I dont have any pets.");
+    counter ++;
+  } else if (ques1 === "yes" || ques1 === "YES") {
+      console.log("yes is incorrect");
+      alert("Wrong! I don't have any pets");
+   } else if (ques1 !== "yes" || "YES" || "no" || "NO"){
+      console.log("incorrect input");
+      alert("incorrect input");
+   };
 
 
-  var ques2 = prompt("Are you going to eat pizza today? \n yes or no");
-   console.log("Answer to ques2" + " " + ques2);
+   var ques2 = prompt("Do I love Redbull?");
+    console.log("Answer to ques2" + " " + ques2);
 
    if (ques2 === "yes" || ques2 === "YES") {
-    console.log("yes is correct");
-    alert("YES!"+ " " + userName + " " + "pizza is always good!!!");
-  } else if (ques2 === "no" || ques2 ==="NO") {
-    console.log("no is incorrect");
-    alert("Lame" + " " + userName + " " + "pizza is always good!")
-  } else if (game !== "yes" || "YES" || "no" || "NO"){
-    console.log("incorrect input");
-    alert("incorrect input");
-  };
+      console.log("yes is correct");
+      alert("YES!"+ " " + userName + " " + "Redbull is life!!");
+      counter ++;
+    } else if (ques2 === "no" || ques2 ==="NO") {
+       console.log("no is incorrect");
+       alert("You don't know me at all.  I love Redbull")
+    } else if (game !== "yes" || "YES" || "no" || "NO"){
+       console.log("incorrect input");
+       alert("incorrect input");
+    };
 
-   var ques3 = prompt("Are you going to go get pizza right now? \n yes or no");
+   var ques3 = prompt("Is black my favorite color?");
     console.log("Answer to ques3" + " " + ques3);
 
    if (ques3 === "yes" || ques3 === "YES") {
      console.log("yes is correct");
-     alert("Right on!" + " "+ userName + " " + "Go get some pizza!");
+     alert("Yup! All black errthang");
+     counter ++;
    } else if (ques3 === "no" || ques3 === "NO"){
-     console.log("no is incorrect");
-     alert("Bummer" + " " + userName);
+      console.log("no is incorrect");
+      alert("Wrong! Black is my favorite color.");
    } else if (game !== "yes" || "YES" || "no" || "NO"){
-     console.log("incorrect input");
-     alert("incorrect input");
+      console.log("incorrect input");
+      alert("incorrect input");
    };
+
+   alert(userName + " " + "you got" + " " + counter + " " + "out of 3 right!")
+
+   
